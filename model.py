5ce60b10158b2ba7e4921e6362763e66ccbf0901
jhpruda <jhprudames@gmail.com>
2026-01-31 21:02:00 +0000

    created simple neural network.

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 """
+
 A script containing the model which I will build and train on the training data in the data folder.
+
 """
 
 import numpy as np
@@ -13,3 +15,41 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_log_error
 from torch.utils.data import Dataset, DataLoader
+
+class RegressionNN(nn.Module):
+
+    """
+
+    A simple neural network regression module, using nn.Module functionality.
+
+    """
+
+    def __init__(self, input_dim: int) -> None:
+
+        """
+        
+        Initialises the model. The number of input dimensions can be changed
+        depending on the length of the input data set.
+
+        """
+
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        """
+        
+        Completes a forward pass through the neural network.
+
+        """
+
+        return self.model(x)
